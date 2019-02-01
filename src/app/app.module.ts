c134b339f7889eeb2b5def488eaa2ed18988b4ea
Daniele Comandini <Daniele.Comandini@version1.com>
2019-02-01 12:09:49 +0000

    [DC] Font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -48,7 +49,8 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot( appRoutes, { enableTracing: true } ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
